08ae97e63a59c34f4104f895e2f9c598cecde8a7
Shubham soni <ss4185667@gmail.com>
2024-12-26 19:00:23 +0530

    project setup

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">About Us</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-bold">Welcome to Home</h1>
+    </div>
+  );
+};
+
+export default Home;
