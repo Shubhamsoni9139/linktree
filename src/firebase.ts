81461aec2f448b0ac0623cc0db570bad993eaab5
Shubham soni <ss4185667@gmail.com>
2024-12-27 12:28:43 +0530

    gitingonare

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGkMjoDku5mTLFlwXsowr5MAzYVjbQEJU",
-  authDomain: "linkme-fd10e.firebaseapp.com",
-  projectId: "linkme-fd10e",
-  storageBucket: "linkme-fd10e.firebasestorage.app",
-  messagingSenderId: "221099013066",
-  appId: "1:221099013066:web:c6334e24db0cb0cfc69533",
-  measurementId: "G-T9BK8YKTHK"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
